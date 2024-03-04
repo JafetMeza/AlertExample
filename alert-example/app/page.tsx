a7a2b6c00c9b5746cbc69068e326ddb28741071d
Abraham Meza <abraham.meza@sciodesk.com>
2024-03-04 14:14:31 -0600

    ADD alert page example

--- alert-example/app/page.tsx
+++ alert-example/app/page.tsx
@@ -1,95 +1,73 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import ButtonIcon from "@/components/1-atoms/a_ButtonIcon";
+import Input from "@/components/1-atoms/a_input";
+import RadioButton from "@/components/1-atoms/a_radioButton";
+import { ToastIcon } from "@/components/1-atoms/a_toast";
+import { AlertContext } from "@/contexts/AlertContext";
+import { useContext, useRef, useState } from "react";
 
-export default function Home() {
+export default function Home(): React.JSX.Element {
+  const input = useRef<HTMLInputElement>(null);
+  const { CreateToast } = useContext(AlertContext);
+  const EmptyString: string = "";
+  const [error, setError] = useState(EmptyString);
+  const [icon, setIcon] = useState(ToastIcon.success);
+
+  const onRadioButtonClick = (value: string): void => {
+    if (value === "Success") setIcon(ToastIcon.success);
+    else if (value === "Warning") setIcon(ToastIcon.warning);
+    else if (value === "Info") setIcon(ToastIcon.info);
+    else if (value === "Danger") setIcon(ToastIcon.error);
+  };
+
+  const onWatchAlert = (): void => {
+    if (input.current?.value === EmptyString) {
+      setError("Fill the input");
+      return;
+    }
+
+    CreateToast(input.current?.value ?? EmptyString, icon);
+    setError(EmptyString);
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <>
+      <h2 className="title is-2">Alert project example</h2>
+
+      <div className="box p-5">
+        <div className="columns">
+          <div className="column is-two-thirds">
+            <label className="label">Test Alerts</label>
+            <Input
+              ref={input}
+              icon="user"
+              placeholder="Write the message"
+              name="testInput"
+              error="This input can not be empty"
             />
-          </a>
+          </div>
+          <div className="column">
+            <RadioButton
+              data={["Success", "Warning", "Info", "Danger"]}
+              onClick={onRadioButtonClick}
+              name="radioButton"
+              className="is-flex is-flex-direction-column"
+              defaultValue="Success"
+            />
+          </div>
         </div>
+
+        <ButtonIcon
+          icon="user"
+          onClick={onWatchAlert}
+          name="button"
+          className="is-success"
+        >
+          Submit form
+        </ButtonIcon>
+
+        <div className="has-text-danger">{error}</div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
